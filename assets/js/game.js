5d9b75961fb0750cd86e7ac0c5b2dc832db20f07
rajahaseebfayyaz <rajahaseebfayyaz@gmail.com>
2021-11-28 20:49:49 +0000

    fixed bugs in indentation also made end page to fix elements

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,5 +1,5 @@
 const question = document.querySelector('#question');
-const choices = Array.from(document.querySelectorAll('.choices-text')
+const choices = Array.from(document.querySelectorAll('.choices-text'))
 const progressText = document.querySelector('#progressText');
 const scoreText = document.querySelector('#score');
 const progressBarFull = document.querySelector('#progressBarFull');
@@ -134,4 +134,62 @@ let questions = [
 
 
 ]
+
+const SCORE_POINTS = 100
+const MAX_QUESTIONS = 4
+
+startGame = () => {
+    questionCounter = 0
+    score = 0
+    avaialbleQuestions = [...questions]
+    getNewQuestion()
+}
+
+getNewQuestion = () => {
+    if(availableQuestions.lenght === 0 || questionsCounter > MAX_QUESTIONS) {
+        localStorage.setitem(`mostRecentStore`, score) 
+        return window.location.assign(`/end.html`)
+    }
+
+    questionCounter++
+    progressText.innerText = "Question ${questionCounter} of ${MAX_QUESTIONS}"
+    progressBarFull.getElementsByClassName.width = `${(questionCounter/MAX_QUESTIONS) * 100}%`
+    const questionsIndex = Math.floor(Math.random() * availableQuestions.lenght)
+    currentQuestion = availableQuestions[questionsIndex]
+    question.innerText = currentQuestion.question
+
+    choices.forEach(choice => {
+        const number = choice.dataset[`number`]
+        choice.innertext = currentQuestion[`choice` + number]
+    })
+
+    availableQuestions.splice(questionsIndex, 1)
+    
+    AcceptingAnswers = true
+}
+
+choices.forEach(choice => {
+    choice.addEventListener(`click`, e => {
+        if(!acceptingAnswers) return
+
+        acceptingAnswers = false
+        const selectedChoice = e.target
+        const selectedAsnswer = selectedChoice.dataset[`number`]
+        
+        let classToApply = selectedAsnswer == currentQuestion.answer ? `correct` :
+        `incorrect`
+
+        if(classToApply === `correct`) {
+            incrementScore(SCORE_POINTS)
+        }
+
+        selectedChoice.parentElement.classList.add(classToApply)
+
+        setTimeout(() => {
+            selectedChoice.parentElement.classlist.remove(classToApply)
+            getNewQuestion()
+
+        }, 1000)
+    })
+})
 
